26905c5d7a2e425c23873ea7032acf6bc15bb00c
Francesco <f.esposito1997@gmail.com>
2017-01-25 11:33:00 +0100

    Project initialization

---
+++ app/src/main/java/android/vm/rununlock/MainActivity.java
@@ -0,0 +1,17 @@
+package android.vm.rununlock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Francesco on 25/01/2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
